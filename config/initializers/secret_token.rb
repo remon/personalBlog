422dc9c45d12216f93f7fb318cd80495d466ffb3
Remon <remmony@gmail.com>
2018-11-17 03:42:15 +0200

    add index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemonBlog::Application.config.secret_token = '2449244d08a32b384a8c0baf6c0ae45bf48c075681fb16e998f6e6d64c36297f6e62e523471b0a2bc9b40841a936424c952670ef8eac2f7345ed04980756bae4'
